b49ae105f687aabaff06751fedf6c8f8048de40d
Frankenstone <dampf_acc@yahoo.com>
2020-04-22 19:32:05 +0200

    add libgpg-error

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -2347,6 +2347,35 @@ $(D)/gnutls: $(D)/bootstrap $(D)/nettle $(ARCHIVE)/$(GNUTLS_SOURCE)
 	$(TOUCH)
 
 #
+# libgpg-error
+#
+LIBGPG_ERROR_VER    = 1.37
+LIBGPG_ERROR_DIR    = libgpg-error-$(LIBGPG_ERROR_VER)
+LIBGPG_ERROR_SOURCE = libgpg-error-$(LIBGPG_ERROR_VER).tar.bz2
+LIBGPG_ERROR_URL    = https://www.gnupg.org/ftp/gcrypt/libgpg-error
+
+$(ARCHIVE)/$(LIBGPG_ERROR_SOURCE):
+	$(DOWNLOAD) $(LIBGPG_ERROR_URL)/$(LIBGPG_ERROR_SOURCE)
+
+$(D)/libgpg-error: $(D)/bootstrap $(ARCHIVE)/$(LIBGPG_ERROR_SOURCE)
+	$(START_BUILD)
+	$(REMOVE)/$(LIBGPG_ERROR_DIR)
+	$(UNTAR)/$(LIBGPG_ERROR_SOURCE)
+	$(CHDIR)/$(LIBGPG_ERROR_DIR); \
+		$(CONFIGURE) \
+			--prefix=/usr \
+			--mandir=/.remove \
+			--infodir=/.remove \
+			--datarootdir=/.remove \
+			--disable-tests \
+			; \
+		$(MAKE); \
+		$(MAKE) install DESTDIR=$(TARGET_DIR)
+	$(REWRITE_LIBTOOL)/libgpg-error.la
+	$(REMOVE)/$(LIBGPG_ERROR_DIR)
+	$(TOUCH)
+
+#
 # glib-networking
 #
 GLIB_NETWORKING_VER_MAJOR = 2.50
