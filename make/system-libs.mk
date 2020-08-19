c85ca040f133b41027a8176a3026bc032d92f519
Frankenstone <dampf_acc@yahoo.com>
2020-08-19 18:47:28 +0200

    libdvbsi: update to 0.39 and url

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -2024,9 +2024,9 @@ $(D)/librtmp: $(D)/bootstrap $(D)/zlib $(D)/openssl $(ARCHIVE)/$(LIBRTMP_SOURCE)
 #
 # libdvbsi++
 #
-LIBDVBSI_VER = 64efce6
+LIBDVBSI_VER = 515a1b6
 LIBDVBSI_SOURCE = libdvbsi-git-$(LIBDVBSI_VER).tar.bz2
-LIBDVBSI_URL = git://git.opendreambox.org/git/obi/libdvbsi++.git
+LIBDVBSI_URL = git://github.com/mtdcr/libdvbsi.git
 LIBDVBSI_PATCH = libdvbsi-git-$(LIBDVBSI_VER).patch
 
 $(ARCHIVE)/$(LIBDVBSI_SOURCE):
