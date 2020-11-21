77f3122f77ca9a439f927ff0f10838c8cf2386aa
Frankenstone <dampf_acc@online.de>
2020-11-21 16:09:04 +0100

    openvpn: update to 2.5.0

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1663,7 +1663,7 @@ $(D)/dvbsnoop: $(D)/bootstrap $(D)/kernel $(ARCHIVE)/$(DVBSNOOP_SOURCE)
 #
 # openvpn
 #
-OPENVPN_VER = 2.4.9
+OPENVPN_VER = 2.5.0
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VER).tar.xz
 
 $(ARCHIVE)/$(OPENVPN_SOURCE):
