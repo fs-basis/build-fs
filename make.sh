5670ff483514be4faf2ea7a8cfbcd5e3d47fb0c2
Frankenstone <dampf_acc@online.de>
2020-11-28 10:10:48 +0100

    make.sh: swapdata change default value now ON

--- make.sh
+++ make.sh
@@ -217,15 +217,15 @@ if [ $BOXTYPE == 'hd51' -o $BOXTYPE == 'h7' -o $BOXTYPE == 'bre2ze4k' ]; then
 	case $5 in
 		[1-2]) REPLY=$5;;
 		*)	echo -e "\nSelect Swap Data and Linux Swap:"
-			echo -e "   \033[01;32m1)  Swap OFF\033[00m"
-			echo -e "   2)  Swap ON"
+			echo -e "   1)  Swap OFF"
+			echo -e "   \033[01;32m2)  Swap ON\033[00m"
 			read -p "Select SWAP support (1-2)? ";;
 	esac
 
 case "$REPLY" in
 	1) SWAPDATA="0";;
 	2) SWAPDATA="1";;
-	*) SWAPDATA="0";;
+	*) SWAPDATA="1";;
 esac
 echo "SWAPDATA=$SWAPDATA" >> config
 fi
