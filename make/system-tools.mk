57fbb5fa55467b1d04019d134696303f382ac896
Frankenstone <dampf_acc@online.de>
2021-01-11 17:42:11 +0100

    wget: update to 1.21.1

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1146,7 +1146,7 @@ $(D)/avahi: $(D)/bootstrap $(D)/expat $(D)/libdaemon $(D)/dbus $(ARCHIVE)/$(AVAH
 #
 # wget
 #
-WGET_VER = 1.20.3
+WGET_VER = 1.21.1
 WGET_SOURCE = wget-$(WGET_VER).tar.gz
 WGET_PATCH = wget-$(WGET_VER).patch
 
