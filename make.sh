3978675aeb0c15b2cc417c96ed3cb6f7cd8fc91c
Frankenstone <dampf_acc@online.de>
2021-02-14 08:10:16 +0100

    make.sh fix space in GCC

--- make.sh
+++ make.sh
@@ -238,10 +238,10 @@ if [ $BOXARCH == 'arm' -o $BOXARCH == 'mips' ]; then
 	case $6 in
 		[1-7]) REPLY=$6;;
 		*)	echo -e "\nSelect GCC version:"
-			echo "   1)  GCC version 6.5.0"
+			echo "   1)  GCC version  6.5.0"
 			echo "   2)  GCC version  7.5.0"
 			echo "   3)  GCC version  8.3.0"
-			echo -e "   \033[01;32m5)  GCC version 8.4.0\033[00m"
+			echo -e "   \033[01;32m5)  GCC version  8.4.0\033[00m"
 			echo "   5)  GCC version  9.2.0"
 			echo "   6)  GCC version  9.3.0"
 			echo "   7)  GCC version 10.2.0"
