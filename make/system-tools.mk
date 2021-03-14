a63524b0188350d268e8ab379ea1268d96028d79
Frankenstone <dampf_acc@online.de>
2021-03-14 16:57:12 +0100

    gptfdisk: update to 1.0.7

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -538,7 +538,7 @@ $(D)/util_linux: $(D)/bootstrap $(D)/zlib $(ARCHIVE)/$(UTIL_LINUX_SOURCE)
 #
 # gptfdisk
 #
-GPTFDISK_VER = 1.0.5
+GPTFDISK_VER = 1.0.7
 GPTFDISK_SOURCE = gptfdisk-$(GPTFDISK_VER).tar.gz
 
 $(ARCHIVE)/$(GPTFDISK_SOURCE):
