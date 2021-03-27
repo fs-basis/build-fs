8cecbccad205bee1fa0059181923043448dbcea3
Frankenstone <dampf_acc@online.de>
2021-03-27 16:14:52 +0100

    patches.mk: make neutrino-plugins-patch

--- make/patches.mk
+++ make/patches.mk
@@ -5,6 +5,9 @@ neutrino%-patch \
 libstb-hal%-patch:
 	( cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ] )
 
+neutrino-plugins-patch:
+	( cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ] )
+
 # keeping all patches together in one file
 # uncomment if needed
 #
