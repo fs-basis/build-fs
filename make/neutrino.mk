4038a262d8d969af28c47ebda8f760c101d36271
Frankenstone <dampf_acc@online.de>
2022-09-15 09:53:45 +0200

    neutrino.mk: lcd4l switch hal lcd4l branch

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -149,7 +149,7 @@ GIT_URL      ?= https://github.com/fs-basis
 NEUTRINO  = neutrino-fs
 LIBSTB_HAL   = libstb-hal-fs
 NMP_BRANCH  ?= lcd4l
-HAL_BRANCH  ?= master
+HAL_BRANCH  ?= lcd4l
 NMP_PATCHES  = $(NEUTRINO_FS_LCD4L_PATCHES)
 HAL_PATCHES  = $(LIBSTB_HAL_FS_PATCHES)
 else ifeq  ($(FLAVOUR), FS_TEST)
