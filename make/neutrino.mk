73c9d5fba4d2773f79ac03439e77428b07ab3f41
Frankenstone <dampf_acc@online.de>
2022-10-20 21:18:19 +0200

    Axas E4HD 4K Ultra added part 4

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -110,6 +110,11 @@ N_CONFIG_OPTS += --enable-flac
 NEUTRINO_DEPS += $(D)/flac
 endif
 
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), e4hdultra))
+N_CONFIG_OPTS += --with-lcddev=/dev/fb1
+N_CONFIG_OPTS += --enable-lcd
+endif
+
 ifeq ($(BOXTYPE), $(filter $(BOXTYPE), e4hdultra vuduo4k vuduo4kse vuuno4kse vuultimo4k vusolo4k))
 N_CONFIG_OPTS += --enable-graphlcd
 NEUTRINO_DEPS += $(D)/graphlcd
