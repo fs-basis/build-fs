836fa7a8fb4f28d45ecacf9493b98102e4db7340
Frankenstone <dampf_acc@online.de>
2022-11-01 18:04:39 +0100

    system-tools: bash remove

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -59,40 +59,6 @@ endif
 	$(TOUCH)
 
 #
-# bash
-#
-BASH_VER = 5.0
-BASH_SOURCE = bash-$(BASH_VER).tar.gz
-BASH_PATCH = $(PATCHES)/bash
-
-$(ARCHIVE)/$(BASH_SOURCE):
-	$(DOWNLOAD) https://ftp.gnu.org/gnu/bash/$(BASH_SOURCE)
-
-$(D)/bash: $(D)/bootstrap $(ARCHIVE)/$(BASH_SOURCE)
-	$(START_BUILD)
-	$(REMOVE)/bash-$(BASH_VER)
-	$(UNTAR)/$(BASH_SOURCE)
-	$(CHDIR)/bash-$(BASH_VER); \
-		$(call apply_patches, $(BASH_PATCH),p0); \
-		$(CONFIGURE) \
-			--prefix=/usr \
-			--mandir=/.remove \
-			--infodir=/.remove \
-			--docdir=/.remove \
-			--localedir=/.remove \
-			--htmldir=/.remove \
-			--disable-nls \
-			--disable-rpath \
-		; \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-		cd $(TARGET_DIR)/bin && rm -f bash && ln -sf /usr/bin/bash bash
-		rm -f $(TARGET_DIR)/usr/bin/bashbug
-		rm -f $(TARGET_LIB_DIR)/bash/{loadables.h,Makefile.inc}
-	$(REMOVE)/bash-$(BASH_VER)
-	$(TOUCH)
-
-#
 # mtd_utils
 #
 MTD_UTILS_VER = 1.5.2
