263f725e410a21b0df46f166fc6ee2361d3c3dca
Frankenstone <dampf_acc@online.de>
2022-11-03 22:54:06 +0100

    neutrino.mk: fix missing sym image-version flashimage.mk: fix e4hdultra flash

--- make/neutrino-release.mk
+++ make/neutrino-release.mk
@@ -148,6 +148,9 @@ neutrino-release-base:
 	cp -a $(TARGET_DIR)/usr/sbin/* $(RELEASE_DIR)/usr/sbin/
 	cp -dp $(TARGET_DIR)/.version $(RELEASE_DIR)/
 	ln -sf /.version $(RELEASE_DIR)/var/etc/.version
+ifeq ($(BOXARCH), $(filter $(BOXARCH), arm))
+	ln -sf /.version $(RELEASE_DIR)/var/etc/image-version
+endif
 	cp $(TARGET_DIR)/boot/$(KERNELNAME) $(RELEASE_DIR)/boot/
 	ln -sf /proc/mounts $(RELEASE_DIR)/etc/mtab
 	cp -dp $(SKEL_ROOT)/sbin/MAKEDEV $(RELEASE_DIR)/sbin/
