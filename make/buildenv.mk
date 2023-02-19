091f8c56a05df5d036ac2478e1d4eda15ef8ad12
max_10 <max_10@gmx.de>
2023-02-19 22:21:12 +0100

    - deleting cortex-strings is not needed

--- make/buildenv.mk
+++ make/buildenv.mk
@@ -64,7 +64,6 @@ TARGET               ?= sh4-linux
 BOXARCH              ?= sh4
 KERNELNAME            = uImage
 TARGET_MARCH_CFLAGS   =
-CORTEX_STRINGS        =
 endif
 
 ifeq ($(BOXARCH), arm)
@@ -74,7 +73,6 @@ TARGET               ?= arm-cortex-linux-gnueabihf
 BOXARCH              ?= arm
 KERNELNAME            = zImage
 TARGET_MARCH_CFLAGS   = -march=armv7ve -mtune=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=hard
-CORTEX_STRINGS        = -lcortex-strings
 endif
 
 OPTIMIZATIONS        ?= size
@@ -136,7 +134,7 @@ TARGET_CFLAGS         +=-fcommon
 endif
 TARGET_CPPFLAGS       = $(TARGET_CFLAGS)
 TARGET_CXXFLAGS       = $(TARGET_CFLAGS)
-TARGET_LDFLAGS        = $(CORTEX_STRINGS) -Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,$(TARGET_LIB_DIR) -L$(TARGET_LIB_DIR) -L$(TARGET_DIR)/lib $(TARGET_EXTRA_LDFLAGS)
+TARGET_LDFLAGS        = -Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,$(TARGET_LIB_DIR) -L$(TARGET_LIB_DIR) -L$(TARGET_DIR)/lib $(TARGET_EXTRA_LDFLAGS)
 LD_FLAGS              = $(TARGET_LDFLAGS)
 PKG_CONFIG            = $(HOST_DIR)/bin/$(TARGET)-pkg-config
 PKG_CONFIG_PATH       = $(TARGET_LIB_DIR)/pkgconfig
