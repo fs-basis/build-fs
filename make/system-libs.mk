f9a1f7db6284ae03d4173d0c6021ef31e5b82b99
BPanther <bpanther_ts@hotmail.com>
2024-05-08 00:32:26 +0200

    pugixml: update to f1d8c51

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1513,7 +1513,7 @@ $(D)/libroxml: $(D)/bootstrap $(ARCHIVE)/$(LIBROXML_SOURCE)
 #
 # pugixml
 #
-PUGIXML_VER = b2b4664
+PUGIXML_VER = f1d8c51
 PUGIXML_PATCH = pugixml-config.patch
 
 $(D)/pugixml: $(D)/bootstrap $(ARCHIVE)/$(PUGIXML_SOURCE)
