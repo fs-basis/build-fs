7d9c2f4a2e228834ee51f6f1b70486fc99350b1f
Frankenstone <dampf_acc@online.de>
2025-10-08 15:01:53 +0200

    ffmpeg: fix for newer os

--- make/ffmpeg.mk
+++ make/ffmpeg.mk
@@ -61,12 +61,6 @@ ifeq ($(BOXARCH), $(filter $(BOXARCH), sh4))
 FFMPEG_CONF_OPTS  += --cpu=generic
 endif
 
-#ifeq ($(BOXARCH), sh4)
-#ifeq ($(AUTOCONF_NEW),1)
-#	FFMPEG2_PATCH = ffmpeg-4.4-sh4.patch
-#endif
-#endif
-
 FFMPRG_EXTRA_CFLAGS  = -I$(TARGET_INCLUDE_DIR)/libxml2
 
 ifeq ($(FFMPEG_SNAPSHOT), 1)
@@ -426,10 +420,6 @@ FFMPRG_EXTRA_CFLAGS =
 ifneq ($(BOXTYPE), $(filter $(BOXTYPE), ufs910 ufs922))
 FFMPEG_CONF_OPTS = --enable-muxer=hevc --enable-parser=hevc --enable-decoder=hevc
 endif
-
-#ifeq ($(AUTOCONF_NEW),1)
-#	FFMPEG2_PATCH = ffmpeg-2.8-sh4.patch
-#endif
 
 $(ARCHIVE)/$(FFMPEG_SOURCE):
 	$(DOWNLOAD) http://www.ffmpeg.org/releases/$(FFMPEG_SOURCE)
