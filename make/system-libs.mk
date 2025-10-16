1cb51532dbdb78c8554f68e479c9fef259ec2e39
BPanther <bpanther_ts@hotmail.com>
2025-10-16 09:49:54 +0200

    pugixml: update to git

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1516,7 +1516,7 @@ $(D)/libroxml: $(D)/bootstrap $(ARCHIVE)/$(LIBROXML_SOURCE)
 #
 # pugixml
 #
-PUGIXML_VER = 61c9448
+#PUGIXML_VER = 61c9448
 PUGIXML_PATCH = pugixml-config.patch
 
 $(D)/pugixml: $(D)/bootstrap $(ARCHIVE)/$(PUGIXML_SOURCE)
