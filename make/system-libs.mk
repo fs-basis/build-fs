3ebaf0d59587c32f7d7abd866931c78b1d4c4e6f
BPanther <bpanther_ts@hotmail.com>
2025-11-22 15:24:35 +0100

    libpng: change version to branch

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -735,7 +735,8 @@ $(D)/libjpeg_turbo: $(D)/bootstrap $(ARCHIVE)/$(LIBJPEG_TURBO_SOURCE)
 #
 # libpng
 #
-LIBPNG_VER = 49363ad
+#LIBPNG_VER = 49363ad
+LIBPNG_VER = libpng16
 LIBPNG_VER_X = 16
 
 $(D)/libpng: $(D)/bootstrap $(D)/zlib
