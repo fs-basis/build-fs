b2a981f296e46a7bd899fa90469c8bfdaca54504
Frankenstone <dampf_acc@online.de>
2026-04-02 15:42:32 +0200

    ffmpeg 8.1 added, master now 8.2-dev , thx BP

--- make.sh
+++ make.sh
@@ -24,7 +24,7 @@ fi
 
 if [ "$1" == -h ] || [ "$1" == --help ]; then
 	echo "Parameter 1                             : Target system (1-70)"
-	echo "Parameter 2 (not UFS910/UFS922)         : FFMPEG Version (1-5)"
+	echo "Parameter 2 (not UFS910/UFS922)         : FFMPEG Version (1-6)"
 	echo "Parameter 3                             : Optimization (1-6)"
 	echo "Parameter 4                             : Neutrino variant (1-2)"
 	echo "Parameter 5                             : External LCD support (1-4)"
@@ -157,13 +157,14 @@ if [ "$BOXARCH" == "sh4" ]; then
 elif [ "$BOXARCH" == "arm" ]; then
 	CNT=0
 	case $2 in
-		[1-5]) REPLY=$2;;
+		[1-6]) REPLY=$2;;
 		*)	echo -e "\nFFMPEG version:"
 			echo -e "   \033[01;32m1)  FFMPEG 4.4    GIT\033[00m" && CNT=$(($CNT+1))
 			echo "   2)  FFMPEG 6.1    GIT [experimental]" && CNT=$(($CNT+1))
 			echo "   3)  FFMPEG 7.1    GIT [experimental]" && CNT=$(($CNT+1))
 			echo "   4)  FFMPEG 8.0    GIT [experimental]" && CNT=$(($CNT+1))
-			echo "   5)  FFMPEG MASTER GIT [experimental]" && CNT=$(($CNT+1))
+			echo "   5)  FFMPEG 8.1    GIT [experimental]" && CNT=$(($CNT+1))
+			echo "   6)  FFMPEG MASTER GIT [experimental]" && CNT=$(($CNT+1))
 			read -p "Select FFMPEG version (1-$CNT)? "
 			;;
 	esac
@@ -173,7 +174,8 @@ elif [ "$BOXARCH" == "arm" ]; then
 		2)  FFMPEG_VER="6.1";;
 		3)  FFMPEG_VER="7.1";;
 		4)  FFMPEG_VER="8.0";;
-		5)  FFMPEG_VER="master";;
+		5)  FFMPEG_VER="8.1";;
+		6)  FFMPEG_VER="master";;
 		*)  FFMPEG_VER="4.4";;
 	esac
 	echo "FFMPEG_VER=$FFMPEG_VER" >> config
